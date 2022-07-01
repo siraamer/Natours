37306dec205f916ac4e298d9914ad5d339a5a365
Muhammad Aamer <79223338+siraamer@users.noreply.github.com>
2022-07-01 23:41:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,7 @@ import fs from 'fs';
 import http from 'http';
 import url from 'url';
 import replaceTemplate from './modules/replaceTemplate.js';
-/////////////////////////////////////
-// !FILES
-// Blocking, Syncronous way
-// const txtIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(txtIn);
-// const txtOut = `This is what we know about the avocado: ${txtIn}.\n Created on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', txtOut);
-// console.log('file written!');
 
-// Non-blocking, asyncronous way
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   if (err) return console.log('Error');
-//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile(`./txt/final.txt`, `${data2} ${data3}`, 'utf-8', (err) => {
-//         console.log('Your File Has Been Written! ');
-//       });
-//     });
-//   });
-// });
-// console.log('will read File');
-
-/////////////////////////////////////
-// !SERVER
 
 const data = fs.readFileSync(`./dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
